4d3901f094a8792552362a4d4f5f6360095fd63d
aadarsh pandey <aadarshongraph64@gmail.com>
2023-07-20 14:55:58 +0530

    add navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'page/home'
+  get 'page/about'
+  get 'page/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'optionselect#index'
+  # root 'optionselect#index'
   #  post 'options/submit'
+
+  root 'page#home'
+   get 'about', to: 'page#about'
+  get 'contact', to: 'page#contact'
 end
