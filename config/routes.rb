fadcdd21e2016956f60ac65f64bbdf96a2536800
aadarsh pandey <aadarshongraph64@gmail.com>
2023-07-20 18:58:52 +0530

    create a model item add css and connect database

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  get 'page/home'
-  get 'page/about'
-  get 'page/contact'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get 'page/home'
+  # get 'page/about'
+  # get 'page/contact'
+  # # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # root 'optionselect#index'
+  # # Defines the root path route ("/")
+  # # root "articles#index"
+  # get 'optionselect#index'
   #  post 'options/submit'
 
-  root 'page#home'
-   get 'about', to: 'page#about'
-  get 'contact', to: 'page#contact'
+  # get 'page#home'
+  #  get 'about', to: 'page#about'
+  # get 'contact', to: 'page#contact'
+
+   resources :items
+  root 'items#index'
 end
